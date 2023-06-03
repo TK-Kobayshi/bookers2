70fa9d640a4179d3a8e101780ca5700fa8791d58
TK-Kobayshi <ec2-user@ip-172-31-39-113.ap-northeast-1.compute.internal>
2023-06-03 09:12:41 +0000

    [Finish]

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+  resources :books
+  root to: "homes#top"
+  get "/home/about" => "homes#about", as: "about"
+
+  resources :users, only: [:show, :edit,:index,:update]
+  resources :books, only: [:new, :index, :show, :edit, :create, :update, :destroy]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
