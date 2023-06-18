9bffb7e0df0e12570405baa166217295866bb4d9
TK-Kobayshi <ec2-user@ip-172-31-39-113.ap-northeast-1.compute.internal>
2023-06-18 03:26:27 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   get "/home/about" => "homes#about", as: "about"
 
   resources :users, only: [:show, :edit,:index,:update]
-  resources :books, only: [:new, :index, :show, :edit, :create, :update, :destroy]
+  resources :books, only: [:new, :index, :show, :edit, :create, :update, :destroy]do
+   resource :favorites, only: [:create, :destroy]
+   resources :book_comments, only: [:create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  end
 end
